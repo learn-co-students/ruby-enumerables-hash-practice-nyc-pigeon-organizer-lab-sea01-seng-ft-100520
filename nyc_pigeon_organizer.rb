ff91f328ef6543e7e8d51c16ccc5dd03490ce1e2
Alexander Grey <alecgrey.coding@gmail.com>
2020-08-12 17:55:01 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -21,22 +21,28 @@ def nyc_pigeon_organizer(data)
 
   new_hash = {}
   
+  # conduct depth-first-search
   data.each do |attr_key, attr_val|
     
     attr_val.each do |type_key, type_val|
       
       type_val.each do |name|
         
+        # add name to new_hash
         if !new_hash.include?(name)
           
-          new_hash[name] = {
-            :color => [],
-            :gender => [],
-            :lives => []
-          }
+          new_hash[name] = {}
           
         end
         
+        # add attribute to name in new_hash
+        if !new_hash[name].include?(attr_key.to_sym)
+          
+          new_hash[name][attr_key.to_sym] = []
+          
+        end
+        
+        # add attribute value to attribute key in nested hash of new_hash
         new_hash[name][attr_key.to_sym] << type_key.to_s
           
       end
