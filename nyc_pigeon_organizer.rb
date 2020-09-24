7e864e59fe938c1cd113b89fba1aeb3d02ea2b11
Kevin Grow <kevingrow11@gmail.com>
2020-09-24 00:40:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  final_results = data.each_with_object({}) do |(key, value), final_hash|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_hash[name]
+          final_hash[name] = {}
+        end 
+        if !final_hash[name][key]
+          final_hash[name][key] = []
+        end 
+       final_hash[name][key].push(inner_key.to_s)
+       end 
+    end
+  end 
+end  
+
+
+
+
+
+
+
+
+
